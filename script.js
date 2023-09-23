674e747183bc5cfe3648666c7255421cc644c8e0
mi8722663079 <mi8722663@gmail.com>
2023-09-23 07:55:11 +0300

    galaxily minor change

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const nums = document.querySelectorAll("[data-id='nums']");
 const signs = document.querySelectorAll("[data-id='signs']");
-const signs = document.querySelector(".screen");
+const screen = document.querySelector(".screen");
