1e2eea77ed96d924a0a92ae61dd10d208f289c5a
mi8722663079 <mi8722663@gmail.com>
2023-09-27 14:21:17 +0300

    finsihed the project

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-const nums = document.querySelectorAll("[data-id='nums']");
-const signs = document.querySelectorAll("[data-id='signs']");
+const buttons = document.querySelectorAll(".buttons");
 const screen = document.querySelector(".screen");
+
+buttons.forEach((num) => {
+  num.addEventListener("click", () => {
+    const number = num.id;
+    if (number == "d") {
+      screen.innerHTML = screen.innerHTML.slice(0, -1);
+    } else if (number == "c") {
+      screen.innerHTML = "";
+    } else if (/[0-9]/g.test(number) || /\+|-|\/|\*|\./g.test(number)) {
+      if (
+        (/[0-9]/g.test(screen.innerHTML) == false &&
+          /\+|-|\/|\*|\./g.test(screen.innerHTML) == false) ||
+        screen.innerHTML.includes("=")
+      ) {
+        screen.innerHTML = "";
+      }
+      screen.innerHTML += number;
+    } else if (number == "=") {
+      screen.innerHTML =
+        eval(screen.innerHTML) == Infinity
+          ? "Rethink your life lol"
+          : `=${eval(screen.innerHTML)}`;
+    }
+  });
+});
